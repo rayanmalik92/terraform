ee775e40779d30b59946dfdea39673cf48154f8d
EC2 Default User <ec2-user@ip-172-31-90-46.ec2.internal>
2024-03-21 01:58:29 +0000

    added main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "Amazon-linux-instance-2" {
+  count            = 1
+  ami              = "ami-0f403e3180720dd7e"
+  instance_type    = "t2.medium"
+  key_name         = "newkey"
+  tags = {
+    name = "New jenkins server"
+  }
+}
