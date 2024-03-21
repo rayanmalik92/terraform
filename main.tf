93f3d8bb660e614b59f4087d962b978fc3708e73
EC2 Default User <ec2-user@ip-172-31-90-46.ec2.internal>
2024-03-21 04:30:32 +0000

    added another instance again

--- main.tf
+++ main.tf
@@ -12,3 +12,12 @@ resource "aws_instance" "Amazon-linux-instance-2" {
   }
 }
 
+resource "aws_instance" "Amazon-linux-instance-3" {
+  count            = 1
+  ami              = "ami-0f403e3180720dd7e"
+  instance_type    = "t2.medium"
+  key_name         = "newkey"
+  tags = {
+    name = "New-jenkins-server-2-2"
+  }
+
